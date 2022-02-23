9b932f18e1b518b2eadcb8c56f61137080f68b9a
Alexander <lalmbah@gmail.com>
2022-02-23 20:52:58 +0300

    initial

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+
+import './../public/style/font.css';
+import './../public/style/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
